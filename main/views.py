fcbd14edb2f2e06655b63bca6005730bf25ac991
Tanay Sengar <tanaysengar@hotmail.com>
2014-04-18 22:55:20 -0500

    Changes for file upload

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from main.models import UPC
+from main.models import UPC, LookupSites
 
 def list_upc(request):
     upc_codes = UPC.objects.all()
     return render_to_response('list_upc.html',{'upc_codes':upc_codes},context_instance=RequestContext(request))
 
-def fluid(request):
-    return render_to_response('fluid.html',{'upc_codes':12},context_instance=RequestContext(request))
+def list_sites(request):
+    lookup_sites = LookupSites.objects.all()
+    return render_to_response('list_sites.html',{'lookup_sites':lookup_sites},context_instance=RequestContext(request))
 
