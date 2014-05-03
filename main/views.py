9d94b225f2c6ffb3f18cc511fcf8a22dfcac6f01
Tanay Sengar <tanaysengar@hotmail.com>
2014-05-03 11:44:25 -0500

    Add more changes

--- main/views.py
+++ main/views.py
@@ -11,13 +11,25 @@ def list_sites(request):
     lookup_sites = LookupSites.objects.all()
     return render_to_response('list_sites.html',{'lookup_sites':lookup_sites},context_instance=RequestContext(request))
 
+
 def upc_reviews(request,code):
     upc_details = UPC.objects.get(pk=code)
-    reviews = CustomerReviews.objects.filter(upc_id=upc_details.id)
+
+    currentsiteid='All'
+
+    if ('lookupsiteid' in request.GET) and (request.GET['lookupsiteid'] !='All'):
+        reviews = CustomerReviews.objects.filter(upc_id=upc_details.id).filter(lookupsite_id=request.GET['lookupsiteid'])
+        currentsiteid = request.GET['lookupsiteid']
+    else:
+        reviews = CustomerReviews.objects.filter(upc_id=upc_details.id)
+
+
     lookup_sites = LookupSites.objects.all()
+
     return render_to_response('upc_reviews.html',{'reviews':reviews,
                                                   'product':upc_details,
                                                   'lookupsites':lookup_sites,
-                                                   'code':code
+                                                   'code':code,
+                                                   'currentsiteid':currentsiteid
                                                   },
                               context_instance=RequestContext(request))
